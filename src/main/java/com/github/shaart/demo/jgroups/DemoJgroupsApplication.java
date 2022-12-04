7403b491c3ced3634e9ef073228ceb7e97754590
Artur Shalaev <imshaart@gmail.com>
2022-12-04 15:28:41 +0300

    initial commit

---
+++ src/main/java/com/github/shaart/demo/jgroups/DemoJgroupsApplication.java
@@ -0,0 +1,13 @@
+package com.github.shaart.demo.jgroups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJgroupsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJgroupsApplication.class, args);
+    }
+
+}
